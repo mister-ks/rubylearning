3baf64594e68e4854fd6b7ea0098c0b3146c61e2
Aneks Varsaista <ks03151103@yahoo.co.jp>
2016-07-01 00:37:30 +0900

    first

---
+++ bigth.rb
@@ -0,0 +1,5 @@
+# encoding:utf-8
+
+name = ["小林","林","高野","森岡"]
+
+puts name.size
---
+++ bonjin.rb
@@ -0,0 +1,9 @@
+# encoding:utf-8
+
+name = ["小林","林","高野","森岡","","凡人"]
+
+i = 0
+while i < 6 do
+  puts name[i]
+  i = i + 1
+end
