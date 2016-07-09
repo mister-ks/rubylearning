b5df0b8d7c3704350c8efa5f4e3569d2fa02793e
Aneks Varsaista <ks03151103@yahoo.co.jp>
2016-07-09 11:09:15 +0900

    learning 1

--- genius.rb
+++ genius.rb
@@ -1,5 +1,7 @@
 # encoding:utf-8
 
-100.times do
-  puts "私は天才だ！"
+def genius
+  100.times do
+    puts "私は天才だ！"
+  end
 end
---
+++ hello.rb
@@ -0,0 +1,11 @@
+# encoding:utf-8
+
+def hello
+  puts "hello,world"
+end
+
+puts hello()
+
+100.times do
+  puts hello()
+end
